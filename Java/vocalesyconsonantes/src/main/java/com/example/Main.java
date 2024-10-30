b50b5752c2718ae813e6c29b7ea09eace652b2f2
MiguelA-Zurita <mzurita@nigul.cide.es>
2024-10-30 17:11:57 +0100

    Remade

--- Java/vocalesyconsonantes/src/main/java/com/example/Main.java
+++ Java/vocalesyconsonantes/src/main/java/com/example/Main.java
@@ -6,11 +6,11 @@ public class Main {
     public static void main(String[] args) {
         String fraseIntroducida = "0";
         int totalVocales = 0;
+        int totalEspacios = 0;
         Scanner sc = new Scanner(System.in);
         System.out.println("Introduce una frase");
-        fraseIntroducida = sc.next();
-        fraseIntroducida = fraseIntroducida.toLowerCase();
-        char[] letras = fraseIntroducida.toCharArray();     
+        fraseIntroducida = sc.nextLine().toLowerCase();
+        char[] letras = fraseIntroducida.toCharArray();  
         for(int i = 0; i<fraseIntroducida.length(); i++){
             switch(letras[i]){
                 case('a'):
@@ -18,14 +18,19 @@ public class Main {
                 case('i'):
                 case('o'):
                 case('u'):
-                System.out.println("La letra " + letras[i] + " es vocal");
-                totalVocales++;
+                    System.out.println("La letra " + letras[i] + " es vocal");
+                    totalVocales++;
                 break;
-            default:
-                System.out.println("la letra " + letras[i] + " es consonante");
+                case('\u0020'):
+                    totalEspacios++;
+                    System.out.println(letras[i] + " es un espacio");
+                    break;
+                default:
+                    System.out.println("la letra " + letras[i] + " es consonante");
             }
             
         }
+        System.out.println("En total hay " + totalEspacios + " espacios");
         System.out.println("En total hay " + totalVocales + " vocales");
         sc.close();
     }
